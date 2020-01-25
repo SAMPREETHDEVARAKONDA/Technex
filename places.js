8da2966f4a70a9ef57dff2103f03948b9177caeb
sampreethdevarakonda <saisampreeth03@gmail.com>
2020-01-25 18:44:10 +0530

    icon add

--- places.js
+++ places.js
@@ -33,32 +33,34 @@ const Locations = [
   },
 ];
 
+
 window.onload = () => {
-  const scene = document.querySelector('a-scene');
+  const scene = document.querySelector("a-scene");
+  return navigator.geolocation.getCurrentPosition(
+    function(position) {
+      Locations.forEach(place => {
+        const latitude = place.latitude;
+        const longitude = place.longitude;
 
-  // first get current user location
-  return navigator.geolocation.getCurrentPosition(function (position) {
-    Locations.forEach((place) => {
-      const latitude = place.latitude;
-      const longitude = place.longitude;
+        const placeText = document.createElement("a-image");
+        placeText.setAttribute(
+          "gps-entity-place",
+          `latitude: ${latitude}; longitude: ${longitude};`
+        );
+        placeText.setAttribute("name", place.placename);
+        placeText.setAttribute("scale", "10 10 10");
+        placeText.setAttribute('src', './assets/map-marker.png');
 
-      // add place icon
-      const icon = document.createElement('a-image');
-      icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-      icon.setAttribute('name', place.placename);
-      icon.setAttribute('src', './assets/map-marker.png');
+        placeText.addEventListener("loaded", () => {
+          window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
+        });
 
-      // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-      icon.setAttribute("scale", "15 15 15");
-
-      icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-
-      const clickListener = function(ev) {
+        const clickListener = function(ev) {
           ev.stopPropagation();
           ev.preventDefault();
 
           const name = ev.target.getAttribute('name');
-
+          alert(name)
           const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
           if (el && el === ev.target) {
@@ -75,73 +77,16 @@ window.onload = () => {
           }
       };
 
-      icon.addEventListener('click', clickListener);
-      
-      scene.appendChild(icon);
-  });
-  },
-      (err) => console.error('Error in retrieving position', err),
-      {
-          enableHighAccuracy: true,
-          maximumAge: 0,
-          timeout: 27000,
-      }
+      placeText.addEventListener('click', clickListener);
+
+        scene.appendChild(placeText);
+      });
+    },
+    err => console.error("Error in retrieving position", err),
+    {
+      enableHighAccuracy: true,
+      maximumAge: 0,
+      timeout: 27000
+    }
   );
 };
-
-// window.onload = () => {
-//   const scene = document.querySelector("a-scene");
-//   return navigator.geolocation.getCurrentPosition(
-//     function(position) {
-//       Locations.forEach(place => {
-//         const latitude = place.latitude;
-//         const longitude = place.longitude;
-
-//         const placeText = document.createElement("a-image");
-//         placeText.setAttribute(
-//           "gps-entity-place",
-//           `latitude: ${latitude}; longitude: ${longitude};`
-//         );
-//         placeText.setAttribute("name", place.placename);
-//         placeText.setAttribute("scale", "15 15 15");
-//         placeText.setAttribute('src', './assets/map-marker.png');
-
-//         placeText.addEventListener("loaded", () => {
-//           window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
-//         });
-
-//         const clickListener = function(ev) {
-//           ev.stopPropagation();
-//           ev.preventDefault();
-
-//           const name = ev.target.getAttribute('name');
-
-//           const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-//           if (el && el === ev.target) {
-//               const label = document.createElement('span');
-//               const container = document.createElement('div');
-//               container.setAttribute('id', 'place-label');
-//               label.innerText = name;
-//               container.appendChild(label);
-//               document.body.appendChild(container);
-
-//               setTimeout(() => {
-//                   container.parentElement.removeChild(container);
-//               }, 1500);
-//           }
-//       };
-
-//       placeText.addEventListener('click', clickListener);
-
-//         scene.appendChild(placeText);
-//       });
-//     },
-//     err => console.error("Error in retrieving position", err),
-//     {
-//       enableHighAccuracy: true,
-//       maximumAge: 0,
-//       timeout: 27000
-//     }
-//   );
-// };
