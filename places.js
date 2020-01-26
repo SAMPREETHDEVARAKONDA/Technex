b393a9236c07194c2e3045365c1a056103e9f7ed
SAMPREETHDEVARAKONDA <59736171+SAMPREETHDEVARAKONDA@users.noreply.github.com>
2020-01-26 17:08:32 +0530

    Update places.js

--- places.js
+++ places.js
@@ -31,6 +31,10 @@ const Locations = [
     latitude: 25.258140,
     longitude: 82.985531
   },
+  {
+    placename : navigator.geolocation.getCurrentPosition(
+    "my position",  
+    )
 ];
 
 window.onload = () => {
